56687f94b5cc99d90f8294df347f2ac47d14073f
maizencarl-boop <maizencarl@gmail.com>
2026-09-11 00:02:11 +0800

    Create theme-loader.js

---
+++ theme/theme-loader.js
@@ -0,0 +1,59 @@
+// CubernixHosting Theme Loader
+
+(function () {
+    "use strict";
+
+    const themeRoot = "theme/";
+
+    const stylesheets = [
+        "cubernix.css",
+        "components/layout.css",
+        "components/sidebar.css",
+        "components/mobile-menu.css",
+        "components/dashboard.css",
+        "components/topbar.css",
+        "components/background.css",
+        "components/settings/settings.css"
+    ];
+
+    const scripts = [
+        "components/sidebar.js",
+        "components/mobile-menu.js",
+        "components/topbar.js",
+        "components/background.js",
+        "components/settings/settings.js",
+        "cubernix.js"
+    ];
+
+    function loadStylesheet(path) {
+        const link = document.createElement("link");
+
+        link.rel = "stylesheet";
+        link.href = themeRoot + path;
+
+        document.head.appendChild(link);
+    }
+
+    function loadScript(path) {
+        const script = document.createElement("script");
+
+        script.src = themeRoot + path;
+        script.defer = true;
+
+        document.body.appendChild(script);
+    }
+
+    function loadTheme() {
+        stylesheets.forEach(loadStylesheet);
+        scripts.forEach(loadScript);
+    }
+
+    if (document.readyState === "loading") {
+        document.addEventListener(
+            "DOMContentLoaded",
+            loadTheme
+        );
+    } else {
+        loadTheme();
+    }
+})();
